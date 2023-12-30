230e1d850747d44849902b410aaa9adb827ac858
macie <macie@users.noreply.github.com>
2023-12-30 22:48:25 +0100

    feat: Add simpler interface for LLM server/client

--- llama/llama.go
+++ llama/llama.go
@@ -4,12 +4,42 @@
 
 package llama
 
+import (
+	"context"
+)
+
+var (
+	defaultServer = Server{Addr: "localhost:24114"}
+	defaultClient = Client{Addr: "localhost:24114"}
 // DefaultOptions represent neutral parameters for interacting with LLaMA model.
-var DefaultOptions = Options{
+	DefaultOptions = Options{
 	ModelPath: "",
 	Seed:      0,
 	Temp:      1,
 	MinP:      0,
+	}
+)
+
+// SetDefault sets default Server.
+func SetDefault(server Server) {
+	defaultServer = server
+	defaultClient = Client{Addr: defaultServer.Addr}
+}
+
+// Serve starts LLM server and returns error if it fails. It is the caller's
+// responsibility to close Server.
+func Serve(ctx context.Context, modelPath string) error {
+	return defaultServer.Start(ctx, modelPath)
+}
+
+// Complete returns a channel with completion results for given string.
+func Complete(ctx context.Context, s string) (chan string, error) {
+	return defaultClient.Complete(ctx, s)
+}
+
+// Close releases all resources used by LLM server.
+func Close() error {
+	return defaultServer.Close()
 }
 
 // Options represent parameters for interacting with LLaMA model.
